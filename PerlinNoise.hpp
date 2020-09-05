eead211306324609647f6db22a3ff06b4bf71585
Philo Kaulkin <Phil0nator@users.noreply.github.com>
2020-09-04 20:02:56 -0700

    _

--- PerlinNoise.hpp
+++ PerlinNoise.hpp
@@ -29,9 +29,20 @@ PerlinNoise::PerlinNoise(unsigned int seed){
 
 	p.resize(256);
 	std::iota(p.begin(), p.end(), 0);
+	std::default_random_engine engine(seed);
+	std::shuffle(p.begin(), p.end(), engine);
+	p.insert(p.end(), p.begin(), p.end());
 
 
 
+
+}
+
+
+double PerlinNoise::lerp(double t, double a, double b){
+
+
+return (1.0f - b)*t + b*a;
 
 
 
